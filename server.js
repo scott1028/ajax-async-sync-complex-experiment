97941225911b03bee9f13ce7ba9f44d5ef71f90d
scott.lan <scott.lan@taisys.com>
2017-11-17 19:08:06 +0800

    test .load in delay load

--- server.js
+++ server.js
@@ -17,6 +17,26 @@ app.get('/:seconds', function (req, res) {
     }, parseInt(req.params.seconds) * 1000);
 });
 
+app.get('/html/:seconds', function (req, res) {
+    console.log(req.params, new Date().getTime() - start);
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<script src="https://code.jquery.com/jquery-1.12.4.js"></script>');
+    res.write(`<script>$(window).load(function(){
+        console.log($('#obj'));
+    });</script>`);
+    res.write('</head>');
+    res.write('<body>');
+    setTimeout(function(){
+        res.write(`Hello World! By ${req.params.seconds} Seconds`);
+        setTimeout(function(){
+            res.write('<div id="obj"></div>')
+            res.write('</body></html>');
+            res.end();
+        }, 1000);
+    }, parseInt(req.params.seconds) * 1000);
+});
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
 });
